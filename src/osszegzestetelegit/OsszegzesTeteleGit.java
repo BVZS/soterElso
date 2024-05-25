1c051222f8c2517498bb839bb6a85d2e2b24d008
Balazs Fajd <mrfajd@gmail.com>
2024-05-25 10:10:41 +0200

    megszamlalas tetele

--- src/osszegzestetelegit/OsszegzesTeteleGit.java
+++ src/osszegzestetelegit/OsszegzesTeteleGit.java
@@ -3,10 +3,11 @@ package osszegzestetelegit;
 
 public class OsszegzesTeteleGit {
 
-    static int[] sorozat = {1, 3, 5}; 
+    static int[] sorozat = {0, 1, 2, 3, 5}; 
     
     public static void main(String[] args) {
         osszegzes();
+        megszamlalas();
     }
 
     private static void osszegzes() {
@@ -14,7 +15,17 @@ public class OsszegzesTeteleGit {
         for (int i = 0; i < sorozat.length; i++) {
             osszead += sorozat[i];
         }
-        System.out.println("[1, 3, 5] összege: " + osszead);
+        System.out.println("[0, 1, 2, 3, 5] összege: " + osszead);
+    }
+
+    private static void megszamlalas() {
+        int db = 0;
+        for (int i = 0; i < sorozat.length; i++) {
+            if (sorozat[i] < 3) {
+                db++;
+            }
+        }
+        System.out.println("A sorozatban " + db + " db 3-nál kisebb elem van.");
     }
     
 }
