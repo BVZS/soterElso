84ea6b418cdb5f9cf82e9f494a418644b23afb12
Balazs Fajd <mrfajd@gmail.com>
2024-05-25 14:59:52 +0200

    minimumTeszt, eldontesTeszt hozzaadva

--- src/teszt/TesztProgTetelekGit.java
+++ src/teszt/TesztProgTetelekGit.java
@@ -12,6 +12,8 @@ public class TesztProgTetelekGit {
         osszegzesTeszt();
         megszamlalasTeszt();
         maximumTeszt();
+        minimumTeszt();
+        eldontesTeszt();
     }
 
     private static void osszegzesTeszt() {
@@ -35,6 +37,22 @@ public class TesztProgTetelekGit {
         int kapott = ProgTetelekGit.maximumKivalasztas(tesztErtekek);
         int vart = 2;
         System.out.println("* maximum teszt (1, 2, 1, 2) = " + kapott);
+        assert kapott == vart : "sikertelen";
+    }
+    
+    private static void minimumTeszt() {
+        int[] tesztErtekek = {1, 2, 1, 2};
+        int kapott = ProgTetelekGit.minimumKivalasztas(tesztErtekek);
+        int vart = 1;
+        System.out.println("* minimum teszt (1, 2, 1, 2) = " + kapott);
+        assert kapott == vart : "sikertelen";
+    }
+
+    private static void eldontesTeszt() {
+        int[] tesztErtekek = {1, 2, 1, 2};
+        boolean kapott = ProgTetelekGit.eldontesLegalabbEgy(tesztErtekek);
+        boolean vart = false;
+        System.out.println("* eldöntés teszt (1, 2, 1, 2) = " + kapott);
         assert kapott == vart : "sikertelen";
     }
 }
