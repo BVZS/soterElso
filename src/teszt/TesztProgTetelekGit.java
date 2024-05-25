bbe493e696254f1cc42a3631c602d86920477fc8
Balazs Fajd <mrfajd@gmail.com>
2024-05-25 14:47:36 +0200

    osszegzesTeszt, megszamlalasTeszt, maximumTeszt hozzaadva

--- src/teszt/TesztProgTetelekGit.java
+++ src/teszt/TesztProgTetelekGit.java
@@ -9,12 +9,32 @@ public class TesztProgTetelekGit {
     }
 
     private static void tesztesetek() {
-        
-        int[] tesztErtekek = {3, 1, 7, 2};
+        osszegzesTeszt();
+        megszamlalasTeszt();
+        maximumTeszt();
+    }
+
+    private static void osszegzesTeszt() {
+        int[] tesztErtekek = {1, 2, 1, 2};
         int kapott = ProgTetelekGit.osszegzes(tesztErtekek);
-        int vart = -1;
-        System.out.println("* összegzés teszt (3, 1, 7, 2)");
+        int vart = 6;
+        System.out.println("* összegzés teszt (1, 2, 1, 2) = " + kapott);
         assert kapott == vart : "sikertelen";
-     
+    }
+
+    private static void megszamlalasTeszt() {
+        int[] tesztErtekek = {1, 2, 1, 2};
+        int kapott = ProgTetelekGit.megszamlalas(tesztErtekek);
+        int vart = 4;
+        System.out.println("* megszámlálás teszt (1, 2, 1, 2) = " + kapott);
+        assert kapott == vart : "sikertelen";
+    }
+
+    private static void maximumTeszt() {
+        int[] tesztErtekek = {1, 2, 1, 2};
+        int kapott = ProgTetelekGit.maximumKivalasztas(tesztErtekek);
+        int vart = 2;
+        System.out.println("* maximum teszt (1, 2, 1, 2) = " + kapott);
+        assert kapott == vart : "sikertelen";
     }
 }
